0b4a39ae8090beb82b52668181fead022cc46c31
Adrielly <islyadrielly@gmail.com>
2022-01-19 16:10:00 -0300

    agora sim ta pronto

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,19 +10,22 @@ export class CartService {
 
   constructor(private http: HttpClient) { }
 
-  addToCart(product: Product) {
+  addToCart(product: Product) { 
+    // adiciona ao carrinho, anexa o item solicitado a um array
     this.items.push(product);
   }
 
   getItems() {
+    // pega os itens adicionados ao carrinho e retorna cada um deles em suas respectivas quantidades
     return this.items;
   }
 
   clearCart() {
+    // limpa o carrinho, retornando o array vazio.
     this.items = [];
     return this.items;
   }
-
+  
     getShippingPrices() {
       return this.http.get<{type: string, price: number}[]> 
       ('/assets/shipping.json');
